12022d0170b6d8ec91ece69c1f49502ab87850dc
denis_kodochigov <kodochigovds@yandex.ru>
2023-06-16 22:59:32 +0300

    2023-06-16 Animate screen

--- app/src/main/java/com/example/shopping_list/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/shopping_list/navigation/AppNavHost.kt
@@ -1,63 +1,77 @@
 package com.example.shopping_list.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import com.example.shopping_list.ui.article.ArticlesScreen
 import com.example.shopping_list.ui.baskets.BasketsScreen
 import com.example.shopping_list.ui.products.ProductsScreen
 import com.example.shopping_list.ui.settings.SettingsScreen
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun AppNavHost(
+fun AppAnimatedNavHost(
     navController: NavHostController,
     modifier: Modifier = Modifier,
     bottomSheetVisible: MutableState<Boolean>,
     bottomSheetContent: MutableState <@Composable (() -> Unit)?>,
     bottomSheetHide: () -> Unit
-) {
+){
 
-    NavHost(navController = navController, startDestination = Baskets.route, modifier = modifier){
+    AnimatedNavHost(
+        navController = navController, startDestination = Baskets.route, modifier = modifier ) {
 
-        composable( route = Baskets.route) {
+        val enterTransition = slideInHorizontally(
+            animationSpec = tween(durationMillis = 1200)) { it / 2 } +
+                fadeIn( animationSpec = tween(durationMillis = 1200))
+        val exitTransition = slideOutHorizontally(
+            animationSpec = tween(durationMillis = 1200)) + fadeOut()
+        composable(route = Baskets.route,
+            enterTransition = { enterTransition },
+            exitTransition = { exitTransition }){
             BasketsScreen(
                 bottomSheetContent = bottomSheetContent,
                 bottomSheetHide = bottomSheetHide,
                 bottomSheetVisible = bottomSheetVisible,
                 onClickBasket = { navController.navigateToProducts(it) },)
         }
-
-        composable( route = ProductsBasket.routeWithArgs, arguments = ProductsBasket.arguments )
+        composable(route = ProductsBasket.routeWithArgs, arguments = ProductsBasket.arguments,
+            enterTransition = { enterTransition },
+            exitTransition = { exitTransition })
         { navBackStackEntry ->
             val basketId = navBackStackEntry.arguments?.getLong(ProductsBasket.basketIdArg)
             if (basketId != null) {
                 ProductsScreen(
                     basketId = basketId,
                     bottomSheetVisible = bottomSheetVisible,
-                    bottomSheetContent = bottomSheetContent)
+                    bottomSheetContent = bottomSheetContent
+                )
             }
         }
-
-        composable( route = Articles.route) {
+        composable(route = Articles.route,
+            enterTransition = { enterTransition },
+            exitTransition = { exitTransition }){
             ArticlesScreen(
                 bottomSheetContent = bottomSheetContent,
                 bottomSheetVisible = bottomSheetVisible,)
         }
-        composable( route = Setting.route) {
+        composable(route = Setting.route,
+            enterTransition = { enterTransition },
+            exitTransition = { exitTransition }){
             SettingsScreen()
         }
-//        composable(
-//            route = SingleAccount.routeWithArgs,
-//            arguments = SingleAccount.arguments,
-//            deepLinks = SingleAccount.deepLinks
-//        ) { navBackStackEntry ->
-//            val accountType = navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-//            SingleAccountScreen(accountType)
-//        }
+
     }
 }
+
+
 
--- app/src/main/java/com/example/shopping_list/ui/MainApp.kt
+++ app/src/main/java/com/example/shopping_list/ui/MainApp.kt
@@ -1,41 +1,54 @@
 package com.example.shopping_list.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FabPosition
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.shopping_list.navigation.*
+import com.example.shopping_list.navigation.AppAnimatedNavHost
+import com.example.shopping_list.navigation.Baskets
+import com.example.shopping_list.navigation.appTabRowScreens
+import com.example.shopping_list.navigation.navigateToScreen
 import com.example.shopping_list.ui.components.BottomBarApp
 import com.example.shopping_list.ui.components.FloatingActionButtonApp
 import com.example.shopping_list.ui.theme.AppTheme
 import com.example.shopping_list.ui.theme.BackgroundBottomSheet
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import kotlinx.coroutines.launch
 
 @SuppressLint("RememberReturnType", "UnrememberedMutableState")
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun MainApp() {
     AppTheme {
-        val navController = rememberNavController()
-        val currentBackStack by navController.currentBackStackEntryAsState()
-        val currentDestination = currentBackStack?.destination
-        val currentScreen = appTabRowScreens.find { it.route == currentDestination?.route } ?: Baskets
         val bottomSheetState = rememberModalBottomSheetState(
             initialValue = ModalBottomSheetValue.Hidden,
             skipHalfExpanded = true,
             animationSpec = tween(durationMillis = 500, delayMillis = 0, easing = FastOutLinearInEasing))
         val bottomSheetContent = remember { mutableStateOf<@Composable (() -> Unit)?>({ })  }
         val scope  = rememberCoroutineScope()
+
+        val animatedNavController  = rememberAnimatedNavController()
+        val animCurrentBackStack by animatedNavController.currentBackStackEntryAsState()
+        val animCurrentDestination = animCurrentBackStack?.destination
+        val animCurrentScreen = appTabRowScreens.find { it.route == animCurrentDestination?.route } ?: Baskets
 
         ModalBottomSheetLayout(
             sheetState  = bottomSheetState,
@@ -47,9 +60,9 @@ fun MainApp() {
                 modifier = Modifier.background(MaterialTheme.colors.background).padding(14.dp),
                 bottomBar = {
                     BottomBarApp(
-                        currentScreen = currentScreen,
+                        currentScreen = animCurrentScreen, //currentScreen,
                         onTabSelection = { newScreen ->
-                            navController.navigateToScreen(newScreen.route) }) },
+                            animatedNavController.navigateToScreen(newScreen.route) }) },
                 floatingActionButton = {
                     FloatingActionButtonApp( onClick = { scope.launch {
                         if (bottomSheetContent.value != null) { bottomSheetState.show() }}})
@@ -57,7 +70,7 @@ fun MainApp() {
                 floatingActionButtonPosition = FabPosition.Center,
                 isFloatingActionButtonDocked = true,
             ) { innerPadding ->
-                AppNavHost(navController = navController,
+                AppAnimatedNavHost( navController = animatedNavController,
                     modifier = Modifier.padding(innerPadding),
                     bottomSheetContent = bottomSheetContent,
                     bottomSheetVisible = mutableStateOf( bottomSheetState.currentValue != ModalBottomSheetValue.Hidden),
