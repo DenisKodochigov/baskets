0593843f0ae4ff230c0abe99af807ed940dbac8a
Denis Kodochigov <kodochigovds@gmail.com>
2023-11-01 17:04:02 +0300

    2023-11-01

--- app/src/main/java/com/example/basket/AppBase.kt
+++ app/src/main/java/com/example/basket/AppBase.kt
@@ -2,10 +2,16 @@ package com.example.basket
 
 import android.app.Application
 import android.content.Context
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AppBase: Application()  {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
+    }
     companion object App{
         var scale: Int = 0
 
--- app/src/main/java/com/example/basket/entity/ErrorApp.kt
+++ app/src/main/java/com/example/basket/entity/ErrorApp.kt
@@ -5,7 +5,6 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import com.example.basket.R
-import com.example.basket.utils.log
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -13,12 +12,9 @@ class ErrorApp @Inject constructor(@ApplicationContext val context: Context) {
 
     fun errorApi (errorMessage:String){
 
-        val toastMessage: String
-        log( true,"Error: $errorMessage")
-
-        when(errorMessage){
-            "error_addProduct" -> toastMessage = context.getString(R.string.error_addProduct)
-            else -> toastMessage = context.getString(R.string.errorUser)
+        val toastMessage = when(errorMessage){
+            "error_addProduct" -> context.getString(R.string.error_addProduct)
+            else -> context.getString(R.string.errorUser)
         }
         if (toastMessage.isNotEmpty()) {
             Handler(Looper.getMainLooper()).post {
